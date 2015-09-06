1e52f2db2de2911444374fc1c30f746befc5b273
flexdigit <georg-org@hotmail.de>
2015-09-06 12:13:51 +0200

    Update Garage_control.ino

--- Garage_control.ino
+++ Garage_control.ino
@@ -11,8 +11,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
+// configure Wifi
 #define SSID "astral"
-#define PW   "12345678"             // set to "" for open access point w/o passwortd
+#define PW   "12345678"                 // set to "" for open access point w/o passwortd
+
+// Oder leiber so?
+const char* password = "mypassphrase";
+
+// configure IO
+const int GPIO0 = 0;                    // GPIO0 used for the relay
+const int GPIO2 = 2;                    // GPIO2 not realy used
  
 String html1 = "<!DOCTYPE html>\r\n<html>\r\n\
                 <head>\r\n<meta content=\"text/html; charset=ISO-8859-1\" http-equiv=\"content-type\">\r\n\
@@ -22,14 +30,14 @@ String html2 = "\">\r\n<input value=\"Open/Close\" style=\"";
 String html3 = " width:15em;height:13em; font-size: 24px;\" type=\"submit\">\
                 </form>\r\n</head>\r\n<body>\r\n</body>\r\n</html>";
  
-ESP8266WebServer server(80);        // Setup server port
-int val = 1;                        // switched off on setup
+ESP8266WebServer server(80);            // Setup server port
+int val = 1;                            // switched off on setup
 String Temp = "";
  
-void Ereignis_Index()               // Executed if "http://<ip address>/" is called
+void Ereignis_Index()                   // Executed if "http://<ip address>/" is called
 {
-    digitalWrite(0, val);           // GPIO0
-    val = !val;                     // doggle relay
+    digitalWrite(GPIO0, val);           // GPIO0
+    val = !val;                         // doggle relay
     
     Serial.print("LED is ");
     Serial.println(val);
@@ -42,31 +50,31 @@ void Ereignis_Index()               // Executed if "http://<ip address>/" is cal
  
 void setup()
 {
-    pinMode(0, OUTPUT);                     // GPIO0 configured as output
-    pinMode(2, INPUT_PULLUP);               // GPIO2 configured as input with Pullup
-    digitalWrite(0, 1);                     // initial condition 1 (Relay off)
+    pinMode(GPIO0, OUTPUT);             // GPIO0 configured as output
+    pinMode(GPIO2, INPUT_PULLUP);       // GPIO2 configured as input with Pullup
+    digitalWrite(GPIO0, 1);             // initial condition 1 (Relay off)
     
-    Serial.begin(115200);                   // init serial inteface
-    Serial.println("");                     // print empty line
-    Serial.print("Starte WLAN-Hotspot >>");
+    Serial.begin(115200);               // init serial inteface
+    Serial.println("");                 // print empty line
+    Serial.print("\nStarte WLAN-Hotspot >>");
     Serial.print(SSID);
     Serial.println("<<");
-    WiFi.mode(WIFI_AP);                     // set access point modus
-    WiFi.softAP(SSID, PW);                  // start as access point
-    delay(500);                             // wait 0,5s
-    Serial.print("IP Adresse ");            // print of the current IP of the server
+    WiFi.mode(WIFI_AP);                 // set access point modus
+    WiFi.softAP(SSID, PW);              // start as access point
+    delay(500);                         // wait 0,5s
+    Serial.print("IP Adresse ");        // print of the current IP of the server
     Serial.println(WiFi.softAPIP());
     
     // treatment of the different events - before server.begin()!!!
     server.on("/", Ereignis_Index);
     
-    server.begin();                       // start the server
+    server.begin();                     // start the server
     Serial.println("HTTP Server gestartet");
 }
  
 void loop()
 {
-    server.handleClient();
+    server.handleClient();              // check for incomming client connections frequently in the main loop:
     delay(500);
 }
 
